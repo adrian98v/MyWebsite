20d8d83427dd4b76bc55a82c7f4dc97b1a402d0e
Adrian Veppo <veppoadrian98@gmail.com>
2025-05-23 16:27:56 -0300

    cambio imagen

--- obj/Debug/net8.0/MyWebsite.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyWebsite.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyWebsite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+690952a7f47517771e55d8bf74b92b96acd73f4e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08df4485feb0ca4e385d000283d361b5877a068a")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyWebsite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyWebsite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
